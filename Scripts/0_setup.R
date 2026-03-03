a6c97e97a4ef1ca9719670c46ba26d1ee5ca867a
Tyler Bonnell <Tyler.Bonnell@co.snohomish.wa.us>
2026-03-02 17:25:38 -0800

    Add 0_setup.R

---
+++ Scripts/0_setup.R
@@ -0,0 +1,4 @@
+# 0_setup.R
+
+# .REnviron File -----
+file.edit(".Renviron") # Add RAW_DEATH_FILES_FOLDER (the filepath where all raw WA DOH CHS Death files are stored after being downloaded from Secure Access Washington)
