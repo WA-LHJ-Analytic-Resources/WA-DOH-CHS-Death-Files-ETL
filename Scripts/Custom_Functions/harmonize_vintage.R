70bd6487e657ff8e71bea815f2863a44b510a753
Tyler Bonnell <Tyler.Bonnell@co.snohomish.wa.us>
2026-03-16 18:12:03 -0700

    Implement data-vintage specific edits/tweaks

--- Scripts/Custom_Functions/harmonize_vintage.R
+++ Scripts/Custom_Functions/harmonize_vintage.R
@@ -65,7 +65,6 @@
 #' - Standardized **schema** (column names),
 #' - All variables coerced to **character**,
 #' - **Provenance** columns at the front,
-#' - For BEDROCK vintages, **crosswalked** variable codes; for WHALES, codes are retained as-is.
 
 harmonize_vintage <- function(
   file_row,
@@ -97,9 +96,30 @@ harmonize_vintage <- function(
   # 2-Schema harmonization (match variable names) -----
 
   if (file_row$system == "BEDROCK") {
-    message(glue(
-      "2-Scehma Harmonization ({file_row$vintage_label}): BEDROCK --> WHALES variable names..."
-    ))
+    ## Implement Annual BEDROCK Data Vintage Tweaks
+
+    ### BEDROCK 2011-2012
+    if (file_row$file_year %in% 2011:2012) {
+      df_raw <- df_raw %>%
+        ## Convert dth_time and inj_time to NA if they don't have 4 digits (HHMM)
+        mutate(across(
+          .cols = c(dth_time, inj_time),
+          .fns = ~ ifelse(str_length(.x) < 4, NA, .x)
+        )) %>%
+        ## Split dth_time and inj_time into hour and minute columns (to prepare for variable renaming step)
+        tidyr::separate_wider_position(
+          data = .,
+          cols = dth_time,
+          widths = c(dth_hour = 2, dth_min = 2),
+          cols_remove = FALSE
+        ) %>%
+        tidyr::separate_wider_position(
+          data = .,
+          cols = inj_time,
+          widths = c(inj_hour = 2, inj_min = 2),
+          cols_remove = FALSE
+        )
+    }
 
     var_rename_map <- build_variable_name_crosswalk(
       df = df_raw,
@@ -113,6 +133,34 @@ harmonize_vintage <- function(
       dplyr::rename(!!!var_rename_map) # Implement renaming of BEDROCK --> WHALES variable names
   } else if (file_row$system == "WHALES") {
     harmonized_vars <- variable_name_cw$to_name
+
+    ## Implement Annual WHALES Data Vintage Tweaks
+
+    ### WHALES 2016
+    if (file_row$file_year == 2016) {
+      df_raw <- df_raw %>%
+        # Fix Name Variations
+        rename(
+          age_type = age_unit, # age_unit --> age_type
+          residence_length_type = residence_length_units # residence_length_units --> residence_length_type
+        ) %>%
+        # Align 2016 Pregnancy data with 2017 & Onwards
+        mutate(
+          pregnancy = case_when(
+            age_years < 10 | age_years > 55 ~ '8',
+            age_years %in% 10:55 & sex == "F" & is.na(pregnancy) ~ '8',
+            TRUE ~ pregnancy
+          )
+        )
+    }
+
+    ### WHALES 2019
+    if (file_row$file_year == 2019) {
+      df_raw <- df_raw %>%
+        rename(
+          residence_length_type = residence_length_units # residence_length_units --> residence_length_type
+        )
+    }
 
     ## Subset to only Harmonized Data Set Variables
     df_schema <- df_raw %>%
@@ -134,10 +182,6 @@ harmonize_vintage <- function(
   # 3-Value harmonization (crosswalk all variable codes) -----
 
   if (file_row$system == "BEDROCK") {
-    message(glue(
-      "3-Value Harmonization ({file_row$vintage_label}): BEDROCK --> WHALES code values..."
-    ))
-
     df_harmonized <- apply_crosswalk(
       df = df_schema,
       crosswalk = variable_code_cw,
