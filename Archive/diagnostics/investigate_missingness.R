be49d5d3bfd60b2caf378b10ce94df3361b9c527
Tyler Bonnell <Tyler.Bonnell@co.snohomish.wa.us>
2026-03-24 15:45:15 -0700

    Properly order variables on heatmap, include full and filtered heatmap

--- Archive/diagnostics/investigate_missingness.R
+++ Archive/diagnostics/investigate_missingness.R
@@ -1,6 +1,5 @@
 # investigate_missingness.R
 ## Note: This script is meant to examine the output: harmonized_data to see if any abnormal patterns in missingness emerge that may be an artifact of our ETL process.
-pacman::p_load(scales)
 
 # Step 1: Calculate Percent Completeness by File Year and Variable ----
 
@@ -20,6 +19,28 @@ complete_by_year <- harmonized_data %>%
     values_to = "pct_complete"
   ) %>%
   mutate(variable = str_remove(variable, "^pcomplete_")) # Strip the "pcomplete_" prefix to keep clean variable names.
+
+
+## Compute the desired variable order once
+levels_order <- complete_by_year %>%
+  distinct(variable) %>%
+  mutate(
+    sort_key = case_when(
+      str_detect(variable, "^record_axis_code_\\d+$") ~
+        paste0(
+          "record_axis_code_",
+          sprintf("%02d", as.integer(str_extract(variable, "\\d+$")))
+        ),
+      TRUE ~ variable
+    )
+  ) %>%
+  arrange(sort_key) %>%
+  pull(variable)
+
+## Assign Proper Ordering of Variables
+complete_by_year <- complete_by_year %>%
+  mutate(variable = factor(variable, levels = levels_order, ordered = TRUE)) %>% # To ensure record_axis_code_# variables are properly ordered
+  arrange(file_year, variable)
 
 # Step 2: Identify Variables that meets or exceeds a defined completeness threshold (for every file_year) -----
 
@@ -54,9 +75,47 @@ complete_by_year_filtered <- complete_by_year %>%
   filter(!str_detect(variable, "record_axis_code"))
 
 
-# Step 4: Heatmap Visualization -----
+# Step 4a: Heatmap Visualization -----
 
 heatmap <- ggplot(
+  complete_by_year %>%
+    mutate(
+      variable = factor(variable, levels = rev(sort(unique(variable)))),
+      pct_label = percent(pct_complete, accuracy = 0.1) # 0.711111 → "71.1%"
+    ),
+  aes(
+    x = file_year,
+    y = variable,
+    fill = pct_complete,
+    text = paste0(
+      "Variable: ",
+      variable,
+      "<br>",
+      "Year: ",
+      file_year,
+      "<br>",
+      "Complete: ",
+      pct_label
+    )
+  )
+) +
+  geom_tile() +
+  scale_fill_viridis_c(
+    limits = c(0, 1),
+    labels = percent_format(accuracy = 1),
+    name = "% complete"
+  ) +
+  labs(
+    title = "Percent Completeness by Year & Variable",
+    x = "Year",
+    y = "Variable"
+  ) +
+  theme_minimal()
+
+
+# Step 4b: Heatmap (Filtered) Visualization -----
+
+heatmap_filtered <- ggplot(
   complete_by_year_filtered %>%
     mutate(
       variable = factor(variable, levels = rev(sort(unique(variable)))),
@@ -91,4 +150,8 @@ heatmap <- ggplot(
   ) +
   theme_minimal()
 
+
+# Step 5: Initiate Plotly Visualizations -----
+
 plotly::ggplotly(heatmap, tooltip = "text")
+plotly::ggplotly(heatmap_filtered, tooltip = "text")
