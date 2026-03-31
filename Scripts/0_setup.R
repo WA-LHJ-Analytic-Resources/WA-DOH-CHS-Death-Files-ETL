5d14db90608a6131c026654c334107df6e0cb637
Tyler Bonnell <Tyler.Bonnell@co.snohomish.wa.us>
2026-03-31 07:44:52 -0700

    Add additional packages

--- Scripts/0_setup.R
+++ Scripts/0_setup.R
@@ -6,7 +6,18 @@ file.edit(".Renviron") # Add RAW_DEATH_FILES_FOLDER (the filepath where all raw 
 # R Packages & Custom Functions -----
 
 # Install/Load R Packages
-pacman::p_load(fs, glue, here, readr, scales, tictoc, tidyverse, writexl)
+pacman::p_load(
+  arrow,
+  fs,
+  glue,
+  here,
+  nanoparquet,
+  readr,
+  scales,
+  tictoc,
+  tidyverse,
+  writexl
+)
 
 
 # Load All Custom functions
