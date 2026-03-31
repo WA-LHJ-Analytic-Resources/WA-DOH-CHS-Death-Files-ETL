bc8dba3fdd4b52f717f8e0db98d61f985f0df30d
Tyler Bonnell <Tyler.Bonnell@co.snohomish.wa.us>
2026-03-31 13:16:08 -0700

    Remove cemetery codes

--- Scripts/0_setup.R
+++ Scripts/0_setup.R
@@ -46,7 +46,7 @@ params$code_sets_folder <- here::here("Resources", "Code Sets")
 # fmt: skip
 {
   ## Code Sets (4_Code_Set_Expansion)
-  code_sets <- c("cemetery", "country", "fips", "nchs_county", "nchs_state", "wa_county", "wa_county_city")
+  code_sets <- c("country", "fips", "nchs_county", "nchs_state", "wa_county", "wa_county_city")
 
   for(set in code_sets){
 
