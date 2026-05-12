bab1cf2c745716813b018ea61c56356ddd8af60f
Jacob Armitage <90886049+Kovshei@users.noreply.github.com>
2026-05-12 10:06:54 -0700

    comment editing

--- Scripts/1_harmonize_death_files.R
+++ Scripts/1_harmonize_death_files.R
@@ -97,11 +97,11 @@ harmonized_data <- bind_rows(harmonized_list, .id = "file_year")
 tictoc::toc()
 
 # Implement Variable Completeness Check -----
-visualize_completeness(
-  df = harmonized_data,
-  completeness_threshold = 1,
-  plotly = TRUE
-)
+# visualize_completeness(
+#   df = harmonized_data,
+#   completeness_threshold = 1,
+#   plotly = TRUE
+# )
 
 # Clean up -----
 rm(
