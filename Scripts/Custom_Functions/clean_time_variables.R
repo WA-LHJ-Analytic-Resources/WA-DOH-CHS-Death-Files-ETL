cdcdf9c8d001d97445b645f0ee01c7cd2f66d6c6
Tyler Bonnell <Tyler.Bonnell@co.snohomish.wa.us>
2026-09-08 17:40:26 -0700

    Refactor/organize clean_time_variables()

--- Scripts/Custom_Functions/clean_time_variables.R
+++ Scripts/Custom_Functions/clean_time_variables.R
@@ -1,19 +1,27 @@
 # clean_time_variables.R
 
-clean_time_variables <- function(df, verbose = FALSE) {
-  # Helper function to conditionally suppress warnings during time parsing
-  parse_time_wrap <- function(x, fmt = "%H%M") {
-    if (verbose == TRUE) {
-      readr::parse_time(x, format = fmt)
-    } else if (verbose == FALSE) {
-      suppressWarnings(readr::parse_time(x, format = fmt))
-    }
-  }
+df <- harmonized_data %>%
+  select(state_file_number, file_year, contains("time"))
 
-  df_formatted <- df %>%
+clean_time_variables <- function(df, verbose = FALSE) {
+  # Step 0: Remove All Punctuation in All Time Variables
+  df <- df %>%
+    mutate(across(
+      .cols = c(
+        time_of_death,
+        time_of_death_hour,
+        time_of_death_minutes,
+        time_of_injury,
+        time_of_injury_hour,
+        time_of_injury_minutes
+      ),
+      ~ str_remove(.x, "[:punct:]")
+    ))
+
+  # Step 1: Normalize Hour & Minute Component Variables
+  df <- df %>%
+    ## Hour Validation: Ensure hour values are 01-23
     mutate(
-      # 0) Normalize Hour/Minute Variables (ensure they are in proper ranges)
-      ## Hour Validation
       across(
         c(
           time_of_death_hour,
@@ -21,16 +29,19 @@ clean_time_variables <- function(df, verbose = FALSE) {
         ),
         ~ ifelse(as.numeric(.x) >= 24, NA_character_, .x)
       ),
-      ## Minute Validation
+      ## Minute Validation: Ensure minute values are 00-59
       across(
         c(
           time_of_death_minutes,
           time_of_injury_minutes
         ),
         ~ ifelse(as.numeric(.x) >= 60, NA_character_, .x)
-      ),
+      )
+    )
 
-      # 1) Pad Hour/Minute Variables with "0"
+  # Step 2: 0 Pad Hour & Minute Component Variables
+  df <- df %>%
+    mutate(
       across(
         c(
           time_of_death_hour,
@@ -39,53 +50,104 @@ clean_time_variables <- function(df, verbose = FALSE) {
           time_of_injury_minutes
         ),
         ~ str_pad(.x, width = 2, side = 'left', pad = "0")
-      ),
+      )
+    )
 
-      # 2) Create Alternative Time of Death & Time of Injury variables (time_of_death_alt, time_of_injury_alt), will be used as a replacement if time_of_death or time_of_injury respectively are NA
-      time_of_death_alt = case_when(
+  # Step 3: Create Time Variables from Hour & Minute Component Variables
+  df <- df %>%
+    mutate(
+      time_of_death_primary = case_when(
+        ## Hour & Minutes available
         !is.na(time_of_death_hour) & !is.na(time_of_death_minutes) ~ paste0(
           time_of_death_hour,
           time_of_death_minutes
         ),
+        ## Hour only available
         !is.na(time_of_death_hour) & is.na(time_of_death_minutes) ~ paste0(
           time_of_death_hour,
           "00"
-        ), # If only _hour variable is available, set as start of the hour (paste on "00" for minutes)
+        ),
         TRUE ~ NA_character_
       ),
-      time_of_injury_alt = case_when(
+      time_of_injury_primary = case_when(
+        ## Hour & Minutes available
         !is.na(time_of_injury_hour) & !is.na(time_of_injury_minutes) ~ paste0(
           time_of_injury_hour,
           time_of_injury_minutes
         ),
+        ## Hour only available
         !is.na(time_of_injury_hour) & is.na(time_of_injury_minutes) ~ paste0(
           time_of_injury_hour,
           "00"
-        ), # If only _hour variable is available, set as start of the hour (paste on "00" for minutes)
-        TRUE ~ NA_character_
-      ),
-
-      # 3) Coalesce Time of Death & Time of Injury variables (1st using original time_of_death and time_of_injury variables --> (if NA) fill in using alternates)
-      time_of_death = coalesce(time_of_death, time_of_death_alt),
-      time_of_injury = coalesce(time_of_injury, time_of_injury_alt),
-
-      # 5) Normalize "2400" timestamp to "0000"
-      across(
-        c(
-          time_of_death,
-          time_of_injury
         ),
+        TRUE ~ NA_character_
+      )
+    )
+
+  # Step 4: Coalesce Time variables
+  ## Note: Using _primary versions first and backfilling with regular time variables. Rationale: A large majority of death data vintages only provide Hour & Minute component variables (it is the preferred format)
+  df <- df %>%
+    mutate(
+      time_of_death = coalesce(time_of_death_primary, time_of_death),
+      time_of_injury = coalesce(time_of_injury_primary, time_of_injury)
+    )
+
+  # Step 5: Normalize "2400" timestamp to "0000"; COnvert "9999" to NA
+  df <- df %>%
+    mutate(
+      across(
+        .cols = c(time_of_death, time_of_injury),
         ~ ifelse(.x == "2400", "0000", .x)
       ),
+      across(
+        .cols = c(time_of_death, time_of_injury),
+        ~ ifelse(.x == "9999", NA, .x) # 9999 = very common NA placeholder value
+      )
+    )
 
-      # 6) Parse Time of Death & Time of Injury variables to time data type
+  # Step 6: Parse Time Variables (to time data type)
+  df <- df %>%
+    mutate(
       time_of_death_final = parse_time_wrap(time_of_death, fmt = "%H%M"),
       time_of_injury_final = parse_time_wrap(time_of_injury, fmt = "%H%M")
-    ) %>%
-    # 7) Remove unnecessary variables
+    )
+
+  # Step 7: Generate Parsing Error Report
+  parsing_error_examples1 <- df %>%
     select(
-      -time_of_death_alt,
-      -time_of_injury_alt,
+      state_file_number,
+      time_of_death,
+      time_of_death_primary,
+      time_of_death_hour,
+      time_of_death_minutes,
+      time_of_death_final
+    ) %>%
+    distinct(time_of_death, time_of_death_primary, .keep_all = TRUE) %>%
+    filter(
+      (!is.na(time_of_death_primary) | !is.na(time_of_death)) &
+        is.na(time_of_death_final)
+    )
+
+  parsing_error_examples2 <- df %>%
+    select(
+      state_file_number,
+      time_of_injury,
+      time_of_injury_primary,
+      time_of_injury_hour,
+      time_of_injury_minutes,
+      time_of_injury_final
+    ) %>%
+    distinct(time_of_injury, time_of_injury_primary, .keep_all = TRUE) %>%
+    filter(
+      (!is.na(time_of_injury_primary) | !is.na(time_of_injury)) &
+        is.na(time_of_injury_final)
+    )
+
+  # Step 8: Remove Unecessary Variables & Rename Variables
+  df <- df %>%
+    select(
+      -time_of_death_primary,
+      -time_of_injury_primary,
       -time_of_death,
       -time_of_death_hour,
       -time_of_death_minutes,
@@ -93,11 +155,10 @@ clean_time_variables <- function(df, verbose = FALSE) {
       -time_of_injury_hour,
       -time_of_injury_minutes,
     ) %>%
-    # 8) Rename variables
     rename(
       time_of_death = time_of_death_final,
       time_of_injury = time_of_injury_final
     )
 
-  return(df_formatted)
+  return(df)
 }
