041eab4d25a6f2865df8dc606fee2ed98bc2d6bb
Tyler Bonnell <Tyler.Bonnell@co.snohomish.wa.us>
2026-09-09 14:53:03 -0700

    Add parse error examples to audits$ list

--- Scripts/3_clean_harmonized_data.R
+++ Scripts/3_clean_harmonized_data.R
@@ -29,12 +29,12 @@ harmonized_data <- harmonized_data %>%
 ## Clean Date Variables
 cleaned_date_output <- clean_date_variables(df = harmonized_data)
 harmonized_data <- cleaned_date_output$df_clean
-# date_parse_errors <- cleaned_date_output$parsing_errors # Contains all unique examples of when date string parsing failed (i.e. values in final harmonized data set are NA)
+# audits$date_parse_errors <- cleaned_date_output$parsing_errors # Contains all unique examples of when date string parsing failed (i.e. values in final harmonized data set are NA)
 
 ## Clean Time Variables
 cleaned_time_output <- clean_time_variables(df = harmonized_data)
 harmonized_data <- cleaned_time_output$df_clean
-# time_parse_errors <- cleaned_time_output$parsing_errors # Contains all unique examples of when time string parsing failed (i.e. values in final harmonized data set are NA)
+# audits$time_parse_errors <- cleaned_time_output$parsing_errors # Contains all unique examples of when time string parsing failed (i.e. values in final harmonized data set are NA)
 
 ## Load in Final Harmonized Data Schema
 schema_data_types <- readr::read_csv(
